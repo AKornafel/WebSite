d1e361171794902a1c54a45d36d9a245877c0d48
AKornafel <57195434+AKornafel@users.noreply.github.com>
2019-10-30 18:58:54 -0400

    Add files via upload

---
+++ mysitee/ja.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	console.log('the doc is ready');
+	
+	$('#hid').fadeIn(2000);
+	$('#b1').fadeIn(5000);
+	$('#b2').fadeIn(5000);
+	$('#b3').fadeIn(5000);
+
+	
+
+});
